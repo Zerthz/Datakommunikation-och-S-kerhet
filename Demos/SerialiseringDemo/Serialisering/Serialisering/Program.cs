b8efaf2ca21e5ab780eacd5eee4f4f5d7dac0d23
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-19 13:53:21 +0200

    Serialization Demo

---
+++ Demos/SerialiseringDemo/Serialisering/Serialisering/Program.cs
@@ -0,0 +1,18 @@
+﻿using Serialisering;
+using System.Numerics;
+using System.Text.Json;
+
+var instance = new ComplexObject
+{
+    Name = "Felix",
+    Age = 27
+};
+
+// sändaren
+var json = JsonSerializer.Serialize(instance);
+Console.WriteLine($"JSON = {json}");
+
+// mottagaren
+var deserialize = JsonSerializer.Deserialize<ComplexObject>(json);
+Console.WriteLine($"Deserialized.name={deserialize.Name}\nDeserialized.age={deserialize.Age}");
+Console.ReadLine();
