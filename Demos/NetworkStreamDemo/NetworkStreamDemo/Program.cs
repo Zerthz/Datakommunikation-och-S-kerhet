36f829d0545b45a415c06a5ae406c19be80144a7
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-19 15:47:15 +0200

    Stream Second Demo
    this might not work at all.
    But it will also not work for us due to
    no server spinning yet. Will get a server
    tomorrow

---
+++ Demos/NetworkStreamDemo/NetworkStreamDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Net.Sockets;
+using System.Text;
+
+using var tcpClient = new TcpClient();
+await tcpClient.ConnectAsync("localhost", 5005);
+
+using var stream = tcpClient.GetStream();
+
+var text = Console.ReadLine();
+var buffer = Encoding.UTF8.GetBytes(text);
+await stream.WriteAsync(buffer,0, buffer.Length, CancellationToken.None);
+
+var result = new byte[1024];
+int i = 0;
+
+while (stream.CanRead)
+{
+    var value = stream.ReadByte();
+    if (value == -1)
+        break;
+
+    result[i] = (byte)value;
+    i++;
+}
+
+var response = Encoding.UTF8.GetString(result.Take(i + 1).ToArray());
+
+Console.WriteLine(response);
+Console.ReadLine();
