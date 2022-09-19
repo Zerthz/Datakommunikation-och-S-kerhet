25685d4162a2a3c8c232a38d6b1c77e7421e1a9b
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-19 11:37:20 +0200

    Init

---
+++ Demos/SocketsDemo/SocketsDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var endpoint = new IPEndPoint(IPAddress.Loopback, 5005);
+
+// socket är en logisk abstraktion av en port
+using var socket = new Socket(
+    endpoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp
+    );
+
+socket.Bind(endpoint);
+socket.Listen();
+
+Console.WriteLine("Listening..");
+
+var handler = await socket.AcceptAsync();
+while (true)
+{
+
+}
+
+Console.ReadLine();
