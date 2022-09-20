1f6f9c1327a0bae4e817e71255b065e16c8b79fd
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-20 10:33:19 +0200

    Server-Client lab in progress
    Replies with a ? first.
    Locks down the stream after replying,
    doesn't feel right

---
+++ Demos/TCPCReplyEcho/TCPCReplyEcho/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+using var tcpClient = new TcpClient();
+await tcpClient.ConnectAsync("localhost", 5005);
+
+using var stream = tcpClient.GetStream();
+
+var reader = new StreamReader(stream, Encoding.UTF8);
+var writer = new StreamWriter(stream, Encoding.UTF8);
+
+var input = Console.ReadLine();
+// writer.WriteAsync
+await writer.WriteAsync(input);
+// writer.FlushAsync
+await writer.FlushAsync();
+
+while (true) { 
+    byte[] receiveBuffer = new byte[1024];
+    int bytesReceived = stream.Read(receiveBuffer);
+    string data = Encoding.UTF8.GetString(receiveBuffer.AsSpan(0, bytesReceived));
+
+    Console.WriteLine($"Echo: {data}");
+    Console.ReadLine();
+}
+
+Console.WriteLine("Broken");
---
+++ Demos/TCPCReplyEcho/TCPReplyEcho.Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var endpoint = new IPEndPoint(IPAddress.Loopback, 5005);
+using var socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+socket.Bind(endpoint);
+socket.Listen();
+
+Console.WriteLine("Listening on port " + endpoint.Port);
+
+var handler = await socket.AcceptAsync(CancellationToken.None);
+
+Console.WriteLine("Accepted connection");
+
+while (true)
+{
+    var buffer = new byte[2048];
+    var lengthReceived = await handler.ReceiveAsync(buffer, SocketFlags.None);
+   
+    var data = buffer.Take(lengthReceived).ToArray();
+
+    // handler.SendAsync();
+    var response = Encoding.UTF8.GetString(data);
+    Console.WriteLine(response);
+
+    var foo = await handler.SendAsync(data, SocketFlags.None);
+    Console.WriteLine(foo);
+
+   
+
+
+}
