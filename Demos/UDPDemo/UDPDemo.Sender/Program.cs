61ee7c42754bdc0730fb22679c2cbb5312800f9f
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-20 11:13:20 +0200

    Demo Peer 2 Peer UDPClient
    Demo with listener and sender client-client

---
+++ Demos/UDPDemo/UDPDemo.Sender/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+// Det här är skickaren
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var endpoint = new IPEndPoint(IPAddress.Loopback, 15005);
+using var client = new UdpClient(endpoint);
+
+
+
+string message = "Hello World!";
+byte[] bytes = Encoding.UTF8.GetBytes(message);
+
+await client.SendAsync
+    (bytes,
+    new IPEndPoint(IPAddress.Loopback, 15006),CancellationToken.None);
+
+Console.WriteLine($"Sending {message} to port 15006");
+await client.ReceiveAsync();
---
+++ Demos/UDPDemo/UDPDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Vi vill ha 2 klienter portar 15005 - 15006
+// client 1 skickar till 15006 och client 2 lyssnar
+
+// Det här är lyssnaren
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var endpoint = new IPEndPoint(IPAddress.Loopback, 15006);
+using var client = new UdpClient(endpoint);
+
+    Console.WriteLine($"Listening on port: {endpoint.Port}..");
+
+    var result = await client.ReceiveAsync();
+    Console.WriteLine(
+        $"Recieved message " +
+        $"\"{string.Join(",", Encoding.UTF8.GetString(result.Buffer))}\"" +
+        $" from " +
+        $"{result.RemoteEndPoint.Port}");
+   
