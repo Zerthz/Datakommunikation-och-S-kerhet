24fab6578e04adbfc963b884bf51f1559943767f
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-20 13:37:20 +0200

    Websocket Demo

---
+++ Demos/WebSocketDemo/WebSocketDemo/Controllers/WsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace WebSocketDemo.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class WsController : ControllerBase
+    {
+        private readonly ILogger<WsController> _logger;
+
+        public WsController(ILogger<WsController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet("/ws")]
+        public async Task Get()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                await Reader(webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+
+        private async Task Reader(WebSocket webSocket)
+        {
+            while (!webSocket.CloseStatus.HasValue)
+            {
+                var buffer = new byte[2048];
+                var content = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+
+                var s = Encoding.UTF8.GetString(buffer, 0, content.Count);
+                _logger.LogInformation(s);
+
+            }
+
+            await webSocket.CloseAsync(
+                    webSocket.CloseStatus.Value,
+                webSocket.CloseStatusDescription,
+                CancellationToken.None);
+        }
+    }
+}
