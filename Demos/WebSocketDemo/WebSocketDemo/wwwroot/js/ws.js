1c0dc43be176238a331c6415fb01c64f509a6e02
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-20 13:59:29 +0200

    JS for client

---
+++ Demos/WebSocketDemo/WebSocketDemo/wwwroot/js/ws.js
@@ -0,0 +1,11 @@
+﻿let clientId = `Client${Math.random()}`
+
+const socket = new WebSocket("ws://localhost:5156/ws");
+
+socket.onopen = e => {
+    console.log("Socket open", e);
+
+    setInterval(() => {
+        socket.send(`Hej från ${clientId}`);
+    }, 1000)
+};
