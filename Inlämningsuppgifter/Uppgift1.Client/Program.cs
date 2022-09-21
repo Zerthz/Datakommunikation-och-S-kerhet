3a5c2b65fdfa5ba11b67457567a8984261da6c6b
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-21 12:38:33 +0200

    Inlämningsuppgift 1 klar
    Eventuellt behöver ändra streamwritern.
    Det funkar nu, men det är utan streamwriter, hade varit
    bra att ha en fungerande streamwriter

---
+++ Inlämningsuppgifter/Uppgift1.Client/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using Uppgift1.Domain;
+
+using var tcpClient = new TcpClient();
+await tcpClient.ConnectAsync("localhost", 69);
+
+using var stream = tcpClient.GetStream();
+
+//var reader = new StreamReader(stream, Encoding.UTF8);
+//var writer = new StreamWriter(stream, Encoding.UTF8);
+
+
+var felixObj = new DataClass { Name = "Felix", Age = 27 };
+var jsonFelix = JsonSerializer.Serialize(felixObj);
+var jsonFelixBytes = Encoding.UTF8.GetBytes(jsonFelix);
+await stream.WriteAsync(jsonFelixBytes,CancellationToken.None);
+
+//await writer.WriteAsync(jsonFelix);
+//await writer.FlushAsync();
+
+
+
+while(true)
+{
+    byte[] buffer = new byte[1024];
+    int bytesRecieved = stream.Read(buffer);
+    string jsonData = Encoding.UTF8.GetString(buffer.AsSpan(0, bytesRecieved));
+    var deserData = JsonSerializer.Deserialize<DataClass>(jsonData);
+
+    if (deserData == null)
+        throw new NullReferenceException();
+
+    Console.WriteLine($"Message recieved from server:" +
+                            $"\n    Name: {deserData.Name}" +
+                            $"\n    Age: {deserData.Age}" +
+                            $"\n    Message: {deserData.Message}");
+
+
+}
+
