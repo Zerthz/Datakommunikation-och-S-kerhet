9b12cb24233ba548e47e1a22f440784311742e2b
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-22 18:51:45 +0200

    Add structure for UDP flow
    This was just helpful when I was debugging inl2.
    Being able to fill in what goes where is neat for brain

--- Demos/UDPDemo/UDPDemo.Sender/Program.cs
+++ Demos/UDPDemo/UDPDemo.Sender/Program.cs
@@ -9,21 +9,26 @@ var endpoint = new IPEndPoint(IPAddress.Loopback, 15005);
 using var client = new UdpClient(endpoint);
 
 
-
+// Skapa Meddelande
 string message = "Hello World!";
 byte[] bytes = Encoding.UTF8.GetBytes(message);
 
+// Skicka meddelande
+Console.WriteLine($"Sending {message} to port 15006");
 await client.SendAsync
     (bytes,
-    new IPEndPoint(IPAddress.Loopback, 15006),CancellationToken.None);
+    new IPEndPoint(IPAddress.Loopback, 15006),
+    CancellationToken.None);
 
-Console.WriteLine($"Sending {message} to port 15006");
-
+// Ta emot svar
 var result = await client.ReceiveAsync();
+
+// Skriv svaret
 Console.WriteLine(
     $"Recieved message " +
     $"\"{string.Join(",", Encoding.UTF8.GetString(result.Buffer))}\"" +
     $" from " +
     $"{result.RemoteEndPoint.Port}");
 
+// Blocka
 await client.ReceiveAsync();
--- Demos/UDPDemo/UDPDemo/Program.cs
+++ Demos/UDPDemo/UDPDemo/Program.cs
@@ -10,16 +10,23 @@ using System.Text;
 var endpoint = new IPEndPoint(IPAddress.Loopback, 15006);
 using var client = new UdpClient(endpoint);
 
+// Ta emot meddelande
 Console.WriteLine($"Listening on port: {endpoint.Port}..");
-
 var result = await client.ReceiveAsync();
+
+// Skriv ut meddelande
 Console.WriteLine(
     $"Recieved message " +
     $"\"{string.Join(",", Encoding.UTF8.GetString(result.Buffer))}\"" +
     $" from " +
     $"{result.RemoteEndPoint.Port}");
 
+// Skapa svar
 var message = "Thanks for your letter, reading now";
+
+// Skicka svar
 await client.SendAsync(Encoding.UTF8.GetBytes(message),
     result.RemoteEndPoint);
+
+// Blocka
 await client.ReceiveAsync();    
