55a42f060e385a5099463d0694da0ec8f3ee5081
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-26 14:44:41 +0200

    .

---
+++ Demos/SignalRDemo/Web/wwwroot/js/client.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+/// <reference path="signalr/dist/browser/signalr.js" />
+
+var connection =
+    new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:5233/TestHub")
+        .withAutomaticReconnect()
+        .build()
+
+connection.start()
+    .then(async () => {
+        console.log("Connected!");
+
+        const response = await connection.invoke("Complex", {
+            name: "Felix",
+            Age: 27
+        });
+
+        console.log("RESPONSE ", response);
+    })
+    .catch(e => {
+        console.log(e);
+    });
+
+connection.on("Log", (message) => {
+    console.log(message);
+});
+
