c6caf4683f570933d0479e0609de055458cb696a
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-26 10:36:34 +0200

    Demo SignalR intro

---
+++ Demos/SignalRDemo/Client/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.SignalR.Client;
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:5233/TestHub")
+    .WithAutomaticReconnect()
+    .Build();
+
+Console.WriteLine("Väntar");
+Console.ReadLine();
+
+await connection.StartAsync();
+
+Console.WriteLine("Connected!");
---
+++ Demos/SignalRDemo/Server/Hubs/TestHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Server.Hubs
+{
+    public class TestHub : Hub
+    {
+        public TestHub()
+        {
+                
+        }
+    }
+}
