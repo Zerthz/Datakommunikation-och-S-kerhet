fb95c055cba85ccdcea260f8b5c917903188b399
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-26 14:44:29 +0200

    Demo SignalR klar
    Gör de två ToDo i TestHub.cs

--- Demos/SignalRDemo/Server/Hubs/TestHub.cs
+++ Demos/SignalRDemo/Server/Hubs/TestHub.cs
@@ -2,6 +2,11 @@
 
 namespace Server.Hubs
 {
+    public class ComplexObject
+    {
+        public string Name { get; set; }
+        public int Age{ get; set; }
+    }
     public class TestHub : Hub
     {
         private readonly ILogger<TestHub> _logger;
@@ -18,6 +23,19 @@ namespace Server.Hubs
             // Hubben invokear en metod som heter log som finns i clientens context
             Clients.All.SendAsync("Log", message);
         }
+        public ComplexObject Complex(ComplexObject co)
+        {
+            return new ComplexObject
+            {
+                Name = co.Name,
+                Age = co.Age * 2
+            };
+        }
+
+        // Todo SKAPA EN METOD FÖR ATT LÄGGA TILL I GRUPP
+
+        // Todo SKAPA EN METOD FÖR ATT SKICKA TILL GRUPP
+
 
         public override Task OnConnectedAsync()
         {
