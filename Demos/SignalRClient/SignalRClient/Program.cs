5e4f764c0c3b780596f10499ee4388f62ba2886d
Felix <56971417+Zerthz@users.noreply.github.com>
2022-09-26 11:08:59 +0200

    Client to talk with Server from SignalRDemo

---
+++ Demos/SignalRClient/SignalRClient/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.SignalR.Client;
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:5233/TestHub")
+    .WithAutomaticReconnect()
+    .Build();
+
+Console.WriteLine("Väntar");
+Console.ReadLine();
+
+await connection.StartAsync();
+connection.Closed += async reason =>
+{
+    Console.WriteLine(reason + " hände");
+};
+connection.Reconnecting += async status =>
+{
+    Console.WriteLine(status);
+};
+connection.Reconnected += async status =>
+{
+    Console.WriteLine("");
+};
+
+
+
+Console.WriteLine("Connected!");
+
+Console.ReadLine();
