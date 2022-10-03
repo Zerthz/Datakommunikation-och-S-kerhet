92ae15338434d45d801d83e66b53f861b834533b
Felix <56971417+Zerthz@users.noreply.github.com>
2022-10-03 15:07:06 +0200

    BROKEN Demo
    This demo is broken and nonfunctioning. TODO go through
    slides and do the net7.0 rc1 workaround and do that

---
+++ Demos/AuthDemo/Server/Hubs/TestHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Server.Hubs
+{
+    [Authorize]
+    public class TestHub : Hub
+    {
+        [Authorize("OnlyAllowName")]
+        public int Restricted()
+        {
+            return 42;
+        }
+    }
+}
---
+++ Demos/AuthDemo/Server/Program.cs
@@ -0,0 +1,41 @@
+using Server.Auth;
+using Server.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("OnlyAllowFelix", builder =>
+    {
+        builder.AddRequirements(new OnlyAllowNameRequirement("Felix"));
+    });
+});
+builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+
+
+builder.Services.AddSignalR();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+
+app.MapControllers();
+app.MapHub<TestHub>("/Test");
+app.Run();
