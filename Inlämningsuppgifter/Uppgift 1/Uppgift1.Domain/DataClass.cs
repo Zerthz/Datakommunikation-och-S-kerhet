e71c25616873056547510fec21d323d4312e700f
Felix <56971417+Zerthz@users.noreply.github.com>
2022-10-13 13:22:16 +0200

    Commented code

--- Inlämningsuppgifter/Uppgift 1/Uppgift1.Domain/DataClass.cs
+++ Inlämningsuppgifter/Uppgift 1/Uppgift1.Domain/DataClass.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace Uppgift1.Domain
 {
+    // Jag hade ingen större lust att skapa upp exakt samma klass på båda sidor av connectionen, så det här projektet & den här klassen
+    // är endast här för att göra saker lite simplare för en uppgift. 
     public class DataClass
     {
         public string? Name { get; set; }
